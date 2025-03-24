72d420cee613c80c293a7d5d321cb724aa33506e
Devin Ledesma <dev.dfledesma@gmail.com>
2025-03-23 20:24:09 -0500

    Project08-01

--- week-1/project08-01/project08-01.js
+++ week-1/project08-01/project08-01.js
@@ -4,19 +4,47 @@
       Project 08-01
 
       Project to create a timer object
-      Author: 
-      Date:   
+      Author: Devin Ledesma
+      Date:   03/23/2025
 
       Filename: project08-01.js
 */
 
 /*--------------- Object Code --------------------*/
 
+/* Constructor function for timer */
+function timer(min, sec) {
+  timer.minutes = min;
+  timer.seconds = sec;
+  timer.timeID = null;
 
 
+  // Method to start or pause timer
+  timer.prototype.runPause = function(timer, minBox, secBox)
+  {
+    if (this.timeID) {
+      window.clearInterval(this.timeID);
+      this.timeID = null;
+    } else {
+      this.timeID = window.setInterval(countdown, 1000);
+    }
+  }
 
+  function countdown() {
+    if (timer.seconds > 0) {
+      timer.seconds--;
+    } else if (timer.minutes > 0) {
+      timer.minutes--;
+      timer.seconds = 59;
+    } else {
+      window.clearInterval(timer.timeID);
+      timer.timeID = null;
+    }
 
-
+    minBox.value = timer.minutes;
+    secBox.value = timer.seconds
+  }
+}
 
 /*---------------Interface Code -----------------*/
 
@@ -25,3 +53,17 @@ let minBox = document.getElementById("minutesBox");
 let secBox = document.getElementById("secondsBox");
 let runPauseTimer = document.getElementById("runPauseButton");
 
+
+let myTimer = new timer(minBox.value, secBox.value);
+
+minBox.onchange = function () {
+  myTimer.minutes = minBox.value;
+}
+
+secBox.onchange = function() {
+  myTimer.seconds = secBox.value;
+}
+
+runPauseTimer.onclick = function () {
+  myTimer.runPause(myTimer, minBox, secBox);
+}
