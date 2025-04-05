96ae8fe241e279186362709e83c5b4d75b69217f
Devin Ledesma <dev.dfledesma@gmail.com>
2025-04-04 19:20:09 -0500

    Ready to Sumbit

--- week-3/project09-02/project09-02b.js
+++ week-3/project09-02/project09-02b.js
@@ -4,14 +4,13 @@
       Project 09-01
 
       Project to read field values from session storage
-      Author: 
-      Date:   
+      Author:
+      Date:
 
       Filename: project09-02b.js
 */
 
 /* Page Objects */
-
 let riderName = document.getElementById("riderName");
 let ageGroup = document.getElementById("ageGroup");
 let bikeOption = document.getElementById("bikeOption");
@@ -21,4 +20,11 @@ let region = document.getElementById("region");
 let miles = document.getElementById("miles");
 let comments = document.getElementById("comments");
 
-
+riderName.innerText = JSON.parse(sessionStorage.getItem('riderName'));
+ageGroup.innerText = JSON.parse(sessionStorage.getItem('ageGroup'));
+bikeOption.innerText = JSON.parse(sessionStorage.getItem('bikeOption'));
+routeOption.innerText = JSON.parse(sessionStorage.getItem('routeOption'));
+accOption.innerText = JSON.parse(sessionStorage.getItem('accOption'));
+region.innerText = JSON.parse(sessionStorage.getItem('region'));
+miles.innerText = JSON.parse(sessionStorage.getItem('miles'));
+comments.innerText = JSON.parse(sessionStorage.getItem('comments'));
