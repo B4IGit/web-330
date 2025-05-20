5338a46da6a568ba08c6572d0b7a7f2f21b6a3f7
Devin Ledesma <dev.dfledesma@gmail.com>
2025-05-20 12:34:09 -0500

    Ready to Submit

---
+++ week-6/chapter/js11.js
@@ -0,0 +1,42 @@
+"use strict";
+/*
+   JavaScript 7th Edition
+   Chapter 11
+   Chapter Case
+
+   Author:
+   Date:
+
+   Filename: js11.js
+
+
+*/
+
+window.addEventListener("load", init);
+
+function init() {
+   // Page Objects
+   let stories = document.getElementById("stories");
+   let news = document.getElementById("news");
+   let sInput = document.getElementById("sInput");
+   let sButton = document.getElementById("sButton");
+   let suggestBox = document.getElementById("suggestBox");
+
+   // Create a request object
+  const xhr = new XMLHttpRequest();
+
+  // Open the request and send it
+  xhr.open('get', 'commentary.html');
+  xhr.send(null);
+
+  // Handle the changing request state
+  xhr.onreadystatechange = function () {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      // Manage the response
+      stories.innerText = xhr.responseText;
+    } else {
+      console.log(`Request failed: ${xhr.statusText}`)
+    }
+  }
+
+}
